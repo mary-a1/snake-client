478cc421bbb4cbf89dc547bd1b673cea7b4e8be6
Maryan <maryanay2000@gmail.com>
2023-01-18 15:15:22 -0500

    Added message upon connection in the call back and writing data to server

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
 const net = require("net");
 
-
-
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541,
@@ -10,13 +8,22 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
-  conn.on("data", (data)=> {
+
+  conn.on("data", (data) => {
     // code that does something when the connection is first established
-    console.log("You ded cuz you idled", data);
+    console.log(data);
   });
+
+  conn.on("connect", () => {
+    // In the call back printing a message for the player to see
+    console.log("We are connected sucessfully");
+    //writing data to server upon connection  
+    conn.write("Name: M.A");
+  });
+  
   return conn;
+  
 };
 console.log("Connecting ...");
 
-module.exports = {connect}
+module.exports = { connect };
